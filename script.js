b7e0b986243ee67a33ed5cac1580acae79485b6d
Augusto Aleixo <augustoaleixo58@gmail.com>
2024-12-10 22:05:22 -0300

    Atualizando

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ function toggleLetter() {
     const letter = document.getElementById("letter");
     letter.classList.toggle("hidden");
   }
+  function toggleLetter() {
+    const letter = document.getElementById("letter");
+    letter.classList.toggle("hidden");
+  }
+  
   
